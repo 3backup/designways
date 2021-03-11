6338828d529d154c508b1cb35eb3e64a0380e549
Marek Mitis <marek.mitis@revolut.com>
2021-03-11 13:39:35 +0100

    update keys

--- src/util/contentfulPosts.ts
+++ src/util/contentfulPosts.ts
@@ -1,18 +1,18 @@
 import { createClient } from "contentful";
 import { Workshop, WorkshopLevel, WorkshopTag } from "../types";
 
-const space = "vjm3j88sc8ek";
-const accessToken = "ErPqiqNslOZ4hFt6xgTRdqDKeAWJhEDHNuV7R3UxZcE";
+const space = process.env.CONTENTFUL_SPACE;
+const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN;
 
 const client = createClient({
   space,
-  accessToken,
+  accessToken
 });
 
 const getResource = async <T>(resourceName: string, orderField: string) => {
   const entries = await client.getEntries({
     content_type: resourceName,
-    order: `fields.${orderField}`,
+    order: `fields.${orderField}`
   });
   if (!entries.items) {
     console.warn(`Error getting Entries for ${resourceName}.`);
