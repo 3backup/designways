38723d95fecf192025fe47379b394a0a43ea75be
Tomasz Osowski <osowski.tom@gmail.com>
2021-03-15 21:54:43 +0100

    login fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import "../styles/main.sass";
 import "../styles/filters.sass";
-import "../styles/addEvent.scss";
 
 function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
