a3d0c71f80804ac3747fb8c1f75f8d5cad6b7a01
Tomasz Osowski <osowski.tom@gmail.com>
2021-03-27 20:26:27 +0100

    class fix

--- src/components/WorkshopItem/index.tsx
+++ src/components/WorkshopItem/index.tsx
@@ -21,7 +21,7 @@ export const WorkshopItem = (props) => {
           <div className="lecture__tags">
             {" "}
             {props.post.tags.map((singleTag) => (
-              <div class="tag__single tag__single--small">
+              <div className="tag__single tag__single--small">
                 {singleTag.fields.name}
               </div>
             ))}
