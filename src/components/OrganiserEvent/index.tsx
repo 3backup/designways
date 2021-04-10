40456e867c49cdae80397bc600f2796b6e303008
Tomasz Osowski <osowski.tom@gmail.com>
2021-04-10 12:35:37 +0200

    main event ready

--- src/components/OrganiserEvent/index.tsx
+++ src/components/OrganiserEvent/index.tsx
@@ -7,9 +7,57 @@ export const OrganiserEvent = () => {
   return (
     <div className="mainEvent">
       <div className="mainEvent__logo">
-        <img src={Logo} alt="" />
+        <img className="mainEvent__logoImg" src={Logo} alt="" />
+        <div className="mainEvent__recommended">Polecane</div>
       </div>
-      <div className="mainEvent__contet">fff</div>
+      <div className="mainEvent__content">
+        <div className="tags">
+          <div className="tags__singleTag">Badania / Research</div>
+          <div className="tags__singleTag">Analityka</div>
+          <div className="tags__singleTag">Product Design</div>
+          <div className="tags__singleTag">Product Management</div>
+          <div className="tags__singleTag">Service Design</div>
+          <div className="tags__singleTag">UX</div>
+        </div>
+        <div className="mainEvent__price">2460,60 zł</div>
+        <div className="mainEvent__title">
+          <div className="mainEvent__title--level">Średniozaawansowany</div>
+          <div className="mainEvent__title--event">
+            Certyfikowane Warsztaty Product Discovery Practitioner
+          </div>
+        </div>
+        <div className="mainEvent__details">
+          <div className="mainEvent__details--singleDetail">
+            <span className="mainEvent__details--span">Prowadzący</span>
+            Michał Reda, Bartosz Narzelski, Adam Bartkiewicz
+          </div>
+          <div className="mainEvent__details--singleDetail">
+            <span className="mainEvent__details--span">Liczba Miejsc</span>
+            20
+          </div>
+          <div className="mainEvent__details--singleDetail">
+            <span className="mainEvent__details--span">Lokalizacja</span>
+            Online
+          </div>
+          <div className="mainEvent__details--singleDetail">
+            <span className="mainEvent__details--span">Data</span>
+            22.04.2021 - 09:00
+          </div>
+          <div className="mainEvent__details--singleDetail">
+            <span className="mainEvent__details--span">Czas Tewania</span>
+            15 h 00 min
+          </div>
+          <div className="mainEvent__details--singleDetail">
+            <span className="mainEvent__details--span">Typ</span>
+            Szkolenie / warsztat
+          </div>
+        </div>
+        <div className="mainEvent__readMore">
+          <a href="#" className="mainEvent__readMore--link">
+            Dowiedz się więcej
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/organiser/product-discovery-pro.tsx
+++ src/pages/organiser/product-discovery-pro.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Head } from "../../components/Head";
 import { OrganiserHeader } from "../../components/OrganiserHeader";
 import { OrganiserEvent } from "../../components/OrganiserEvent";
+import { OrganiserDetails } from "../../components/OrganiserDetail";
 import { Footer } from "../../components/Footer";
 
 export default function organiser() {
