a0bb81bc95d7efbcc32fc8c4d8c41d8481adb142
Tomasz Osowski <osowski.tom@gmail.com>
2021-06-04 18:34:11 +0200

    nothing chagned

--- src/components/SignupForm/hooks.ts
+++ src/components/SignupForm/hooks.ts
@@ -1,26 +1,23 @@
-import { useMutation } from 'react-query'
-import axios from 'axios'
+import { useMutation } from "react-query";
+import axios from "axios";
 
-import { SuggestFormValues } from './types';
+import { SuggestFormValues } from "./types";
 
 export const useForm = () => {
-    const { mutate: sendForm, isLoading: isSending } = useMutation(async (values: SuggestFormValues) => {
-        const formData = new FormData();
-        const { images, ...rest } = values;
-    
-        if(images.length > 0) {
-            formData.append("images", (images[0] as unknown) as Blob);
-        }
+  const { mutate: sendForm, isLoading: isSending } = useMutation(
+    async (values: SuggestFormValues) => {
+      const formData = new FormData();
+      const { images, ...rest } = values;
+      if (images.length > 0) {
+        formData.append("images", (images[0] as unknown) as Blob);
+      }
 
-        return axios.post(
-          "/api/form",
-          formData,
-          {
-            headers: {
-              "Content-Type": "multipart/form-data",
-            },
-          }
-        );
-      })
-      return { sendForm, isSending}
-}
+      return axios.post("/api/form", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+    },
+  );
+  return { sendForm, isSending };
+};
