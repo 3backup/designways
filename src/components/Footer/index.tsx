1e11cffda88ea1bb08ccfa00eeacf9565d7043f8
Tomasz Osowski <osowski.tom@gmail.com>
2021-06-06 11:35:14 +0200

    styled component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,42 +1,123 @@
 import React from "react";
+import Logo from "images/Logo_horizontal_new.svg";
+import InstagramLogo from "images/instagram-dark.svg";
+import FacebookLogo from "images/facebook-dark.svg";
+import styled from "styled-components";
 
-import Logo from 'images/Logo_horizontal_new.svg'
-import InstagramLogo from 'images/instagram-dark.svg'
-import FacebookLogo from 'images/facebook-dark.svg'
+const FooterContainer = styled.footer`
+  min-height: 5rem;
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+  max-width: 119rem;
+  width: 119rem;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  border-top: 1px solid ${({ theme }) => theme.colors.lightgray};
+  padding: ${({ theme }) => theme.space.s} 0;
+  @media (max-width: ${({ theme }) => theme.breakpoints.xs}) {
+    padding: 0 10px;
+    max-width: 96%;
+    width: 96%;
+  }
 
+  @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
+    padding: 0px 2.5rem;
+    max-width: 96%;
+    width: 96%;
+    flex-direction: column-reverse;
+    justify-content: center;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.m}) {
+    max-width: 92%;
+    width: 92%;
+  }
+`;
+const FooterHalf = styled.div`
+  width: 50%;
+  @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+    width: 100%;
+    img {
+      margin: 0 auto;
+      width: 33%;
+    }
+  }
+`;
+const FooterRight = styled(FooterHalf)`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: flex-end;
+  font-family: Manrope;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 1.6rem;
+  line-height: 140%;
+  text-align: center;
+  letter-spacing: 0.02em;
+  color: ${({ theme }) => theme.colors.darkgrey};
+  @media (max-width: ${({ theme }) => theme.breakpoints.s}) {
+    flex-direction: column-reverse;
+    margin: 2rem;
+    width: 100%;
+  }
+`;
+const FooterCopyrights = styled.p`
+  font-family: Manrope;
+  font-style: normal;
+  font-weight: normal;
+  font-size: ${({ theme }) => theme.fonts.label};
+  margin-top: 10px;
+  line-height: 100%;
+  letter-spacing: 0.02em;
+  color: ${({ theme }) => theme.colors.secondarybackground};
+  a {
+    font-family: Manrope;
+    font-style: normal;
+    font-weight: normal;
+    font-size: ${({ theme }) => theme.fonts.label};
+    margin-top: 10px;
+    line-height: 100%;
+    letter-spacing: 0.02em;
+    color: ${({ theme }) => theme.colors.secondarybackground};
+  }
+`;
 export const Footer = () => {
   return (
-    <footer className="container container--big filterFooter">
-      <div className="filterFooter__footerHalf">
+    <FooterContainer>
+      <FooterHalf>
         <img src={Logo} alt="logo-bottom" />
-        <p className="filterFooter__footerCopyrights">
+        <FooterCopyrights>
           All Rights Reserverd © 2021 DesignWays. Skontaktuj się pod:
           <a
             className="filterFooter__footerCopyrights"
-            href="mailto:hello@designways.io"
-          >
+            href="mailto:hello@designways.io">
             hello@deisgnways.io
           </a>
-        </p>
-      </div>
-      <div className="filterFooter__footerHal filterFooter__rightFooter">
+        </FooterCopyrights>
+      </FooterHalf>
+      <FooterRight>
         <p className="filterFooter__footerTerms">
           <a
             href="https://www.designways.io/polityka-prywatnosci.html"
-            className="filterFooter__footerTerms"
-          >
+            className="filterFooter__footerTerms">
             Terms & Privacy
           </a>
         </p>
         <div className="filterFooter__socialMedia">
           <a href="https://www.facebook.com/designwaysconf">
-            <img src={InstagramLogo} alt=""/>
+            <img src={InstagramLogo} alt="" />
           </a>
           <a href="https://www.instagram.com/designwaysconf/">
             <img src={FacebookLogo} alt="" />
           </a>
         </div>
-      </div>
-    </footer>
+      </FooterRight>
+    </FooterContainer>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,104 @@ import type { AppProps } from "next/app";
 import "../styles/main.sass";
 import "../styles/filters.sass";
 import "../styles/organiser.scss";
-
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 import { useRouteChange } from "../hooks/useRouteChange";
+
+const GlobalStyle = createGlobalStyle`
+
+:root{
+  font-size:10px;
+}
+*, *:before, *:after
+    box-sizing: border-box
+
+html, body, div, span, object, iframe, figure, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, code, em, img, small, strike, strong, sub, sup, tt, b, u, i, ol, ul, li, fieldset, form, label, table, caption, tbody, tfoot, thead, tr, th, td, main, canvas, embed, footer, header, nav, section, video
+    margin: 0
+    padding: 0
+    border: 0
+    font: inherit
+    vertical-align: baseline
+    text-decoration: none
+    text-rendering: optimizeLegibility
+    -webkit-font-smoothing: antialiased
+    text-size-adjust: none
+html
+    font-size: 10px
+
+footer, header, nav, section, main
+    display: block
+
+body
+    line-height: 1
+    font-size: 16px
+    font-family: Manrope
+
+ol, ul
+    list-style: none
+
+blockquote, q
+    quotes: none
+
+blockquote:before, blockquote:after, q:before, q:after
+    content: ''
+    content: none
+
+table
+    border-collapse: collapse
+    border-spacing: 0
+
+input
+    border-radius: 0
+
+html
+    scroll-behavior: smooth
+
+`;
+
+const theme = {
+  colors: {
+    lightviolet: "#AE97F5",
+    violet: "#7D4CE5",
+    darkviolet: "#6B32E3",
+    white: "#FFFFFF",
+    lightgray: "#CED3E0",
+    navyblue: "#2A2D50",
+    darkgrey: "#575983",
+    secondarybackground: "#8A8CB9",
+    green: "#189F0D",
+  },
+  fonts: {
+    base: "1.6rem",
+    titlePage: "5.8rem",
+    sectionHeight: "3.8rem",
+    leadeHeight: "2.4rem",
+    biggerBase: "1.8rem",
+    label: "1.4rem",
+  },
+  space: {
+    xs: "2.5rem",
+    s: "6rem",
+    m: "10rem",
+  },
+  breakpoints: {
+    xxs: "320px",
+    xs: "576px",
+    s: "768px",
+    m: "992px",
+    l: "1200",
+  },
+};
 
 const App = ({ Component, pageProps }: AppProps) => {
   useRouteChange();
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 };
 
 export default App;
